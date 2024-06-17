449c211e8bb586b669e7dffb94ebb0275d00f117
Sofie Pon <sonia10367@gmail.com>
2024-06-17 07:12:34 +0300

    18 - colors update

--- app/src/main/java/com/example/q/HistoryActivity.java
+++ app/src/main/java/com/example/q/HistoryActivity.java
@@ -4,7 +4,6 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
